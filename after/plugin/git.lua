7e7d60eea67e97182e9389cfd1e50c9c27fbc436
Your Name <you@example.com>
2023-09-05 18:15:53 +0300

    update IDE config add git , null-ls support

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -1,11 +1,9 @@
-local status, git = pcall(require, "git")
+local status, git = pcall(require, 'git')
 if (not status) then return end
 
-git.setup({
+git.setup {
   keymaps = {
-    -- Open blame window
     blame = "<Leader>gb",
-    -- Open file/folder in git repository
-    browse = "<Leader>go",
+    browse = "<Leader>go"
   }
-})
+}
